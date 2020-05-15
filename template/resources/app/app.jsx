f2dce30a08a6c165758d817f375ab201a47bfd2d
Fox <1025687605@qq.com>
2020-05-15 13:40:49 +0800

    delete route

--- template/resources/app/app.jsx
+++ template/resources/app/app.jsx
@@ -1,16 +1,14 @@
 import * as React from 'react'
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import routes from './routes'
+import Home from './pages/home'
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        {routes.map((r) => {
-          return <Route key={r.path} exact path={r.path} component={require(r.component).default} />
-        })}
+        <Route exact path="/" component={Home} />
+        <Redirect to="/" />
       </Switch>
-      <Redirect to="/" />
     </Router>
   )
 }
